a309675dd51cbc6a47c5df2d6f413e92a2fc6f96
phamtrungkienit02 <2051052067kien@ou.edu.vn>
2022-11-08 05:07:27 +0700

    new

--- SaleApp/app/dao.py
+++ SaleApp/app/dao.py
@@ -1,13 +1,34 @@
-import json
-from app import app
+from app.models import Category, Product, User
+import hashlib
+
 
 def load_categories():
-    with open('%s/data/categories.json' % app.root_path, encoding="utf-8") as f:
-        return json.load(f)
-def load_products():
-    with open('%s/data/products.json' % app.root_path, encoding="utf-8") as f:
-        return json.load(f)
+    return Category.query.all()
 
-if __name__ == '__main__':
-    import os
-    os.ro
+
+def load_products(category_id=None, kw=None):
+    query = Product.query
+
+    if category_id:
+        query = query.filter(Product.category_id == category_id)
+
+    if kw:
+        query = query.filter(Product.name.contains(kw))
+
+    return query.all()
+
+
+def get_product_by_id(product_id):
+    return Product.query.get(product_id)
+
+
+def auth_user(username, password):
+
+    password = str(hashlib.md5(password.strip().encode('utf-8')).digest())
+
+
+    return User.query.filter(User.username.__eq__(username.strip()), User.password.__eq__(password)).first()
+
+
+def get_user_by_id(user_id):
+    return User.query.get(user_id)
